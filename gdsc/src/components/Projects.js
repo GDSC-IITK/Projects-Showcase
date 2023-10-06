22b3c6a5d58e874429cb65f8b62201cba9d5b312
Tousif <tousifchinnu@gmail.com>
2023-10-06 21:35:47 +0530

    Index page

---
+++ gdsc/src/components/Projects.js
@@ -0,0 +1,73 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/1.jpg";
+import projImg2 from "../assets/img/2.jpg";
+import projImg3 from "../assets/img/3.jpg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Google Developer Student Clubs (GDSC) projects encompass a diverse range of innovative initiatives driven by the collaborative efforts of student developers. These projects reflect the GDSC mission to foster technological skills and problem-solving abilities among students while creating a positive impact on their communities and the world. GDSC projects often span various domains, including mobile app development, web applications, machine learning, and more.</p>
+                <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
