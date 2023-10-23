0604498142721c759cc0859ed6a61a6b48dec62e
Kushagra Srivastava <118841840+whizdor@users.noreply.github.com>
2023-10-23 11:01:36 +0530

    Remove About From NavBar

--- gdsc/src/components/NavBar.js
+++ gdsc/src/components/NavBar.js
@@ -41,7 +41,6 @@ export const NavBar = () => {
             <Nav className="ms-auto">
               <Nav.Link href="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-              <Nav.Link href="#home" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>About</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
